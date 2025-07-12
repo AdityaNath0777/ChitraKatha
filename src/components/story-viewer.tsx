8fbd24447861404e2e553f604817238c1598f49d
Aditya_Mishra <adityasaiyansuper@gmail.com>
2025-07-12 23:42:59 +0530

    019: refactor(component): implement all buttons in StoryViewer

--- src/components/story-viewer.tsx
+++ src/components/story-viewer.tsx
@@ -1,14 +1,33 @@
 "use client";
 
 import React from "react";
-import { ContinueButton, PrevButton } from "./story-buttons";
+import {
+  ContinueButton,
+  GoToMainMenuButton,
+  PrevButton,
+  StartStoryButton,
+} from "./story-buttons";
 import { StoryEntry } from "@/lib/schema/stories";
 
 function StoryViewer({ story }: { story: StoryEntry }) {
   return (
-    <div className="story-viewer-container flex flex-col">
+    <div className="story-viewer-container w-full flex flex-col gap-4 max-w-4xl">
+      <GoToMainMenuButton />
+
+      <div>
+        {story.characters.map((char, idx) => (
+          <span key={`story-char-${idx}`} className="text-9xl">
+            {char}
+          </span>
+        ))}
+      </div>
       <p>{story.description}</p>
-      <div className="w-full flex justify-between gap-4 px-4 py-3">
+
+      <div className="flex items-center justify-center w-full">
+        <StartStoryButton onStart={() => {}} />
+      </div>
+
+      <div className="w-full flex justify-between gap-4">
         <PrevButton onPrev={() => {}} />
         <ContinueButton onContinue={() => {}} />
       </div>
