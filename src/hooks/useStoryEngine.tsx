3e2a30c5962160bbaf636f9bf43db3a217a886c6
Aditya_Mishra <adityasaiyansuper@gmail.com>
2025-07-14 02:27:28 +0530

    030: feat(hooks): add useStoryEngine for scene state management

---
+++ src/hooks/useStoryEngine.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { StoryEntry } from "@/lib/schema/stories";
+import { useState } from "react";
+
+export function useStoryEngine(story: StoryEntry) {
+  const [sceneId, setSceneId] = useState<string>("");
+  const [dialogueEnd, setDialogueEnd] = useState<boolean>(false);
+
+  const currentScene = story.data[sceneId];
+
+  const start = () => setSceneId("start");
+
+  const pickChoice = (id: string) => {
+    setSceneId(id);
+    setDialogueEnd(false);
+  };
+
+  const autoContinue = () => {
+    const onlyChoice = currentScene?.choices?.[0];
+    if (onlyChoice) pickChoice(onlyChoice.nextSceneId);
+  };
+
+  const onDialogueEnd = () => {
+    if (
+      currentScene.choices.length === 0 ||
+      currentScene.choices[0].text.toLowerCase() === "continue"
+    ) {
+      autoContinue();
+    } else setDialogueEnd(true);
+  };
+
+  return {
+    sceneId,
+    dialogueEnd,
+    currentScene,
+    start,
+    pickChoice,
+    onDialogueEnd,
+  };
+}
